e3c3ee3d27bbe8b1b336ff1f2460068af18f3f2e
Kely Rufino <87963324+kely-rufino@users.noreply.github.com>
2021-12-09 19:17:30 +0000

    button added

--- src/Components/ArticleProps.jsx
+++ src/Components/ArticleProps.jsx
@@ -2,13 +2,18 @@ import "./Article.css"
 
 
 export const ArticleProps = (props) => {
-    console.log(props)
     const classes = props.backgroundColor !== undefined ? ["article", props.backgroundColor].join(" "): "article"
+    function printProps (){
+        return console.log(props.title, props.subtitle)
+    }
     return (
         <article className={classes}>
             <h1 className={"title"}> {props.title} </h1>
             <h2 className={"subtitle"}> {props.subtitle} </h2>
             <p> {props.content} </p>
+            <button onClick = {printProps}>
+                Click me!
+            </button>
         </article>
     )
 }
